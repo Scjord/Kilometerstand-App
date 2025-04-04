060a9b932b21eadebedf465d707e5f9441f5fa35
Scjord <tobias.hendricks@me.com>
2025-04-04 17:58:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,23 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = "kilometer-cache-v1";
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('my-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        'index.html',
-        'app.js',
-        'styles.css',
-        'icon.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
